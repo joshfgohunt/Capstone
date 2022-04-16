c98cb66330710cb6a938aa463e7ef457714e5c39
Josh Folsom <josh.folsom@yahoo.com>
2022-04-16 15:12:56 -0700

    Updated 4/16

--- Visualize_Data.py
+++ Visualize_Data.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
-def run_zscore(df, cols):
+def run_ttest(df, cols):
     df = df[cols]
     length = len(df)
-    samp = int(length/15)
     print('full length ',length)
-    print('sample size ',samp)
-    print('df Z-Scores')
-    print(stats.zscore(df).sample(samp).mean())
+    sample = df.iloc[::15, :]
+    print('main df ttest', stats.ttest_ind(df, sample))
 
